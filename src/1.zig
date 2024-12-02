1b5480239ae050bcf421a856bbae97c5bfa32f16
Franklin <franklinjaradev@gmail.com>
2024-12-01 23:59:59 -0500

    day 1

---
+++ src/1.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    // Open the file input-1.txt
+    const file = try std.fs.cwd().openFile("src/input-1.txt", .{});
+    defer file.close();
+
+    var left_list = std.ArrayList(u32).init(allocator);
+    defer left_list.deinit();
+
+    var right_list = std.ArrayList(u32).init(allocator);
+    defer right_list.deinit();
+
+    while (try file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize))) |line| {
+        defer allocator.free(line);
+        var iterator = std.mem.splitSequence(u8, line, "  ");
+        var left_num: u32 = undefined;
+        if (iterator.next()) |n| {
+            left_num = try std.fmt.parseInt(u32, std.mem.trim(u8, n, " "), 10);
+        }
+        var right_num: u32 = undefined;
+        if (iterator.next()) |n| {
+            right_num = try std.fmt.parseInt(u32, std.mem.trim(u8, n, " "), 10);
+        }
+
+        try left_list.append(left_num);
+        try right_list.append(right_num);
+    }
+    std.mem.sort(u32, left_list.items, {}, comptime std.sort.asc(u32));
+    std.mem.sort(u32, right_list.items, {}, comptime std.sort.asc(u32));
+
+    var total: u32 = 0;
+    for (left_list.items, right_list.items) |left, right| {
+        if (left > right) {
+            total += (left - right);
+        } else if (right > left) {
+            total += (right - left);
+        } else {
+            total += 0;
+        }
+    }
+
+    std.debug.print("Total Distance: {d}\n", .{total});
+}
